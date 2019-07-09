632615ee0501d9f41b74c29e5aa700d37eeac7b6
KevinM <tianzeminkevin@gmail.com>
2019-07-09 21:41:43 +1000

    added some unrefined logic into display

--- cartscreen.py
+++ cartscreen.py
@@ -1,8 +1,6 @@
-# from shopscreen import item_quantity
-
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, NumericProperty
-
+from kivy.uix.label import Label
 
 class CartScreenLayout(BoxLayout):
     pass
@@ -30,13 +28,21 @@ class CartLayout(BoxLayout):
     @staticmethod
     def display(item_quantity):
         CartLayout.cart.clear_widgets()
+        counter = 0
         for item in item_quantity:
-            name, quantity = item, item_quantity[item]
-            if quantity != 0:
-                CartLayout.cart.add_widget(CartItem(name=name,
-                                                    quantity=quantity,
-                                                    size_hint_y=None,
-                                                    height=50))
+            print(item_quantity[item])
+            if item_quantity[item] != 0:
+                counter += 1
+        if counter == 0:
+            CartLayout.cart.add_widget(Label(text='Your cart is empty'))
+        else:
+            for item in item_quantity:
+                name, quantity = item, item_quantity[item]
+                if quantity != 0:
+                    CartLayout.cart.add_widget(CartItem(name=name,
+                                                        quantity=quantity,
+                                                        size_hint_y=None,
+                                                        height=50))
 
 
 
