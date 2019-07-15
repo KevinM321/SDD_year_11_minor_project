43b11b4d02676a4c8dd5a2a2fa739a737c0d94ab
KevinM <tianzeminkevin@gmail.com>
2019-07-15 10:13:42 +1000

    weird lucky draw function made even weirder, i dont know what im doing

--- luckydrawscreen.py
+++ luckydrawscreen.py
@@ -1,23 +1,41 @@
-import time
+from kivy.clock import Clock
 from random import randint
 
-from kivy.uix.boxlayout import BoxLayout
+from shopscreen import item_data
 
-items = ['res/Images/beef_jerky.png', 'res/Images/energy_max_bar.png',
-         'res/Images/energy_max_protein_shake.png', 'res/Images/energy_max_supplement.png',
-         'res/Images/energy_max_weightloss_shake.png', 'res/Images/gatorade_drink.png',
-         'res/Images/gfuel_drink.png', 'res/Images/mixed_berries.png',
-         'res/Images/non_fat_yogurt.png', 'res/Images/whey_protein.png']
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.image import Image
 
 
 class LuckyDrawScreenLayout(BoxLayout):
 
+    t = 0
+
+    def __init__(self, **kwargs):
+        super(LuckyDrawScreenLayout, self).__init__(**kwargs)
+        self.event = 0
+
+    def change_display(self):
+        item = item_data[randint(0, 9)]
+        self.lucky_draw_display.source = item[3]
+        if self.t < 2:
+            self.t += 0.2
+        else:
+            Clock.unschedule(self.event)
+            popup = BoxLayout(orientation='vertical')
+            popup.add_widget(Label(text=("You've drawn " + item[0]+' !')))
+            popup.add_widget(Image(source=item[3]))
+            popup.add_widget(Label(text='Come back next week for more!'))
+            p = Popup(title='',
+                      content=popup,
+                      size_hint=(.5, .5))
+            p.open()
+
     def lucky_draw(self):
-        t = 0
-        while t < 3:
-            self.lucky_draw_display.source = items[randint(0, 9)]
-            time.sleep(0.3)
-            t += 0.3
+        self.event = Clock.schedule_interval(lambda dt: self.change_display(), 0.2)
+
 
 
 
