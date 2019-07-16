9e712a876fd6df149470a5dd9c0f63e2708c2074
KevinM <tianzeminkevin@gmail.com>
2019-07-16 16:09:55 +1000

    card info and profile improved, card simulation making in progress

--- luckydrawscreen.py
+++ luckydrawscreen.py
@@ -10,7 +10,6 @@ from kivy.uix.image import Image
 
 
 class LuckyDrawScreenLayout(BoxLayout):
-
     t = 0
 
     def __init__(self, **kwargs):
@@ -18,12 +17,18 @@ class LuckyDrawScreenLayout(BoxLayout):
         self.event = 0
 
     def change_display(self):
+        self.lucky_draw_layout.clear_widgets()
+        self.lucky_draw_layout.add_widget(LuckyDrawImage(
+                                                         source='res/Images/question_mark.png'))
         item = item_data[randint(0, 9)]
         self.lucky_draw_display.source = item[3]
         if self.t < 2:
             self.t += 0.2
         else:
             Clock.unschedule(self.event)
+            self.lucky_draw_layout.clear_widgets()
+            self.lucky_draw_layout.add_widget(Label(text='Lucky draw used\n\nCome back next week for more',
+                                                    halign='center'))
             popup = BoxLayout(orientation='vertical')
             popup.add_widget(Label(text=("You've drawn " + item[0]+' !')))
             popup.add_widget(Image(source=item[3]))
@@ -37,7 +42,9 @@ class LuckyDrawScreenLayout(BoxLayout):
         self.event = Clock.schedule_interval(lambda dt: self.change_display(), 0.2)
 
 
+class LuckyDrawImage(Image):
 
-
-
-
+    def __init__(self, **kwargs):
+        # _lucky_draw_display = '_lucky_draw_display'
+        super(LuckyDrawImage, self).__init__(**kwargs)
+        # self.id = _lucky_draw_display
--- profilescreen.py
+++ profilescreen.py
@@ -1,9 +1,23 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+# from kivy.uix.image import Image
 
 
 class ProfileScreenLayout(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(ProfileScreenLayout, self).__init__(**kwargs)
+
+    def change_password(self):
+        pass
+
+    @staticmethod
+    def card_info():
+        popup = Popup(title='',
+                      content=CardPopup(),
+                      size_hint=(.75, .75))
+        popup.open()
+
+
+class CardPopup(BoxLayout):
     pass
-    # def change_password(self):
-
-
-
